f6527e5af0a15f93d56a345a0724eec3ab0d604e
tampered-sin <tenwangyal9@gmail.com>
2026-06-29 01:05:10 +0530

    feat(trades): update TradesModule with image support

--- apps/api/src/trades/trades.module.ts
+++ apps/api/src/trades/trades.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
 import { TradesController } from './trades.controller';
 import { TradesService } from './trades.service';
+import { TradeImageService } from './trades-image.service';
+import { TradeImageController } from './trades-image.controller';
 import { BehavioralService } from '../analytics/behavioral.service';
 import { EventPublisherService } from '../common/services/event-publisher.service';
 import { QueuesModule } from '../queues/queues.module';
 import { SeedModule } from '../seed/seed.module';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
-  imports: [QueuesModule, SeedModule],
-  controllers: [TradesController],
-  providers: [TradesService, BehavioralService, EventPublisherService],
-  exports: [TradesService, BehavioralService],
+  imports: [QueuesModule, SeedModule, StorageModule],
+  controllers: [TradesController, TradeImageController],
+  providers: [TradesService, TradeImageService, BehavioralService, EventPublisherService],
+  exports: [TradesService, TradeImageService, BehavioralService],
 })
 export class TradesModule {}
