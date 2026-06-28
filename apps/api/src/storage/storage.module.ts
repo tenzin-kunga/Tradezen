1cdba967b707506d45810a9482a4022ae6df2105
tampered-sin <tenwangyal9@gmail.com>
2026-06-29 01:00:39 +0530

    feat(storage): add StorageModule

---
+++ apps/api/src/storage/storage.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { CloudinaryProvider } from './cloudinary.provider';
+
+@Module({
+  providers: [
+    { provide: 'StorageProvider', useClass: CloudinaryProvider },
+  ],
+  exports: ['StorageProvider'],
+})
+export class StorageModule {}
